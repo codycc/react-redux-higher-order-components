b224333c7490e051dc2061209ee008e391dd9f58
Cody Condon <codycondon50@gmail.com>
2016-03-31 11:19:26 -0400

    toggle authentication mimic state

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+
 
 class Header extends Component {
   authButton() {
-    return <button>Sign In</button>
+    if (this.props.authenticated) {
+      return <button onClick={() => this.props.authenticate(false)}>Sign Out</button>
+    }
+    return <button onClick={() => this.props.authenticate(true)}>Sign In</button>
   }
   render() {
     return (
@@ -25,4 +31,9 @@ class Header extends Component {
     );
   }
 }
-export default Header;
+
+function mapStateToProps(state) {
+  return {authenticated: state.authenticated}
+}
+
+export default connect(mapStateToProps, actions)(Header);
--- src/reducers/authentication.js
+++ src/reducers/authentication.js
@@ -1,7 +1,7 @@
 import {CHANGE_AUTH} from '../actions/types';
 
 export default function(state = false, action) {
-  switch(switch.type) {
+  switch(action.type) {
   case CHANGE_AUTH:
     return action.payload;
   }
