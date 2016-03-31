6d71fb6f5b121d268f35fd544ccb89c40a72d749
Cody Condon <codycondon50@gmail.com>
2016-03-31 10:57:44 -0400

    add resources route to react router

--- src/components/resources.js
+++ src/components/resources.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Special text
+      <ul>
+      <li>one </li>
+      <li>two</li>
+      <li>three</li>
+      </ul>
+    </div>
+  );
+}
