b0ca3f54e28e4a8a4e17ce3ffcfab8665575880b
Cody Condon <codycondon50@gmail.com>
2016-03-31 11:56:15 -0400

    add componentWillMount to kick user back to homepage if not signed in

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 
 export default function(ComposedComponent){
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount() {
+      if(!this.props.authenticated){
+      this.context.router.push('/');
+      }
+    }
+
     render() {
       return <ComposedComponent {...this.props}/>
     }
   }
+  function mapStateToProps(state) {
+    return {authenticated: state.authenticated};
+  }
 
-
-return Authentication;
+return connect(mapStateToProps)(Authentication);
 }
